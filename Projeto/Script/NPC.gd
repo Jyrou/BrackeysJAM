23ab76717f66cc1f2a9f1227d8021b5d0281b39c
Estêvão Fiorillo <estevao.fiorilo@gmail.com>
2024-09-12 01:33:04 -0300

    atualiza animacao e cria tipagem mood npc

--- Projeto/Script/NPC.gd
+++ Projeto/Script/NPC.gd
@@ -1,14 +1,16 @@
 extends Area2D
 @onready var timer: Timer = $Timer
-@onready var mood: Sprite2D = $Sprite2D
-
+@onready var mood: AnimatedSprite2D = $AnimatedSpriteMood
+@onready var typemood : int = 0
 #Na _ready enquanto a condiçâo não tiver sido criada (sentar na mesa)
 func _ready() -> void:
 	const delay = 3 #Definido na chamada da futura função
 	timer.wait_time = delay
 	timer.start()
+	
 func _on_timer_timeout():
-	if(mood.frame == 1):
-		mood.set_frame(0)
-	elif(mood.frame == 0):
+	if(typemood==0):
+		mood.play("trocaBle")
+		typemood+=1
+	elif(typemood == 1):
 		print("Colocar o shake aqui")
